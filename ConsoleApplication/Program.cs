4905a49ea062c2390148935c8945f40d07d727bb
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2017-02-23 07:42:48 +0100

    Tried to pass an old test, but failed again.

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -24,6 +24,25 @@ namespace ConsoleApplication
             //Console.WriteLine("EfterBubbleSort_Primitive:" + Environment.NewLine + opgaver.GetListElementsAsSting());
             //Console.WriteLine("------------------------------------------------");
 
+            int[] data = { 24, 37, 1, 21, 5, 8, 4, 31, 32, 17 };
+
+            SortOpgaver opgaver = new SortOpgaver();
+            opgaver.FillList(data);
+            opgaver.InsertionSort();
+            List<int> sortedData = opgaver.GetIntListCopy();
+
+            Console.WriteLine("Unsorted data:");
+            for (int i = 0; i < data.Length-1; i++)
+            {
+                Console.Write(data[i] + ", ");
+            }
+            Console.WriteLine("\nSorted data:");
+            for (int i = 0; i < data.Length - 1; i++)
+            {
+                Console.Write(sortedData[i] + ", ");
+            }
+            Console.WriteLine();
+
             Console.ReadLine();
         }
     }
